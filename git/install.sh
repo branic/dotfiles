892b44004027281424e6d79f2af28e777efe6f8e
Brant Evans <bevans@redhat.com>
2022-10-14 15:43:37 -0700

    Fix sed replacement and use consistent formatting

--- git/install.sh
+++ git/install.sh
@@ -2,20 +2,24 @@
 
 set -e
 
-if [ ! -f "${HOME}/.gitconfig-local" ] ; then
-  cp "${DOTFILES_LOCATION}/git/.gitconfig-local" "${HOME}/.gitconfig-local"
+if [ ! -f "${HOME}/.gitconfig-local" ]; then
+    cp "${DOTFILES_LOCATION}/git/.gitconfig-local" "${HOME}/.gitconfig-local"
 
-  echo -n "Enter your full name: ";
-  read -re name
-  sed -i '' "s/YOUR_NAME/${name}/" "${HOME}/.gitconfig-local"
+    if [[ $- == *i* ]]; then
+        echo -n "Enter your full name: "
+        read -re name
+        sed -i "s/YOUR_NAME/${name}/" "${HOME}/.gitconfig-local"
 
-  echo -n "Enter your email address: ";
-  read -re email
-  sed -i '' "s/YOUR_EMAIL_ADDRESS/${email}/" "${HOME}/.gitconfig-local"
+        echo -n "Enter your email address: "
+        read -re email
+        sed -i "s/YOUR_EMAIL_ADDRESS/${email}/" "${HOME}/.gitconfig-local"
 
-  echo -n "Enter your gpg signing key: ";
-  read -re gpgsign
-  sed -i '' "s/YOUR_GPG_SIGNING/${gpgsign}/" "${HOME}/.gitconfig-local"
+        echo -n "Enter your gpg signing key: "
+        read -re gpgsign
+        sed -i "s/YOUR_GPG_SIGNING/${gpgsign}/" "${HOME}/.gitconfig-local"
+    else
+        echo "Update ~/.gitconfig-local with your username, email address, and git gpg signing key"
+    fi
 fi
 
 # Link the main vimrc file
--- vim/install.sh
+++ vim/install.sh
@@ -8,7 +8,6 @@ ln -sf "${DOTFILES_LOCATION}/vim/.vimrc" "${HOME}/.vimrc"
 # Create the ftplugin directory and link language specific config files
 mkdir -p "${HOME}/.vim/ftplugin"
 
-for file in "${DOTFILES_LOCATION}"/vim/.vim/ftplugin/*
-do
+for file in "${DOTFILES_LOCATION}"/vim/.vim/ftplugin/*; do
     ln -sf "${file}" "${HOME}/.vim/ftplugin/."
 done
