37b1ce67d71352128bd7fd9efb32dbba25ad8d24
Brant Evans <bevans@redhat.com>
2022-10-14 15:32:04 -0700

    Adjust git install script

--- git/install.sh
+++ git/install.sh
@@ -5,15 +5,15 @@ set -e
 if [ ! -f "${HOME}/.gitconfig-local" ] ; then
   cp "${DOTFILES_LOCATION}/git/.gitconfig-local" "${HOME}/.gitconfig-local"
 
-  echo "Enter your full name";
+  echo -n "Enter your full name: ";
   read -re name
   sed -i '' "s/YOUR_NAME/${name}/" "${HOME}/.gitconfig-local"
 
-  echo "Enter your email address";
+  echo -n "Enter your email address: ";
   read -re email
   sed -i '' "s/YOUR_EMAIL_ADDRESS/${email}/" "${HOME}/.gitconfig-local"
 
-  echo "Enter your gpg signing key";
+  echo -n "Enter your gpg signing key: ";
   read -re gpgsign
   sed -i '' "s/YOUR_GPG_SIGNING/${gpgsign}/" "${HOME}/.gitconfig-local"
 fi
