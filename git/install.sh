940f48d34c2765bec1ea4d48138b0adb88d8b5da
Brant Evans <bevans@redhat.com>
2022-10-15 09:51:06 -0700

    Fix sed replacement

--- git/install.sh
+++ git/install.sh
@@ -13,6 +13,7 @@ ln -sf "${DOTFILES_LOCATION}/git/.gitignore-global" "${HOME}/.gitignore-global"
 # Copy local only file
 if [ ! -f "${HOME}/.gitconfig-local" ]; then
     cp "${DOTFILES_LOCATION}/git/.gitconfig-local" "${HOME}/.gitconfig-local"
+    echo -e "${_YELLOW_FG}Update ~/.gitconfig-local with your username, email address, and gpg signing key${_RESET}"
 fi
 
 # Replace local config values
@@ -26,21 +27,21 @@ if [ $retVal -ne 0 ]; then
 fi
 
 if [[ -n "${name}" ]]; then
-    sed -iE -e "/name = /s|[# ]*(name = ).*|\1${name}|" "${HOME}/.gitconfig-local"
+    sed -i -E -e "/name = /s|[# ]*(name = ).*$|\1${name}|" "${HOME}/.gitconfig-local"
 fi
 
 echo -n "Enter your email address: "
 read -re email
 
 if [[ -n "${email}" ]]; then
-    sed -iE -e "/email = /s|[# ]*(email = ).*|\1${email}|" "${HOME}/.gitconfig-local"
+    sed -i -E -e "/email = /s|[# ]*(email = ).*$|\1${email}|" "${HOME}/.gitconfig-local"
 fi
 
 echo -n "Enter your signing key: "
 read -re gpgsignkey
 
 if [[ -n "${gpgsignkey}" ]]; then
-    sed -iE -e "/signingkey = /s|[# ]*(signingkey = ).*|\1${gpgsignkey}|" "${HOME}/.gitconfig-local"
+    sed -i -E -e "/signingkey = /s|[# ]*(signingkey = ).*$|\1${gpgsignkey}|" "${HOME}/.gitconfig-local"
 
-    sed -iE -e '/gpgsign = /s|false$|true|' "${HOME}/.gitconfig-local"
+    sed -i -E -e '/gpgsign = /s|false$|true|' "${HOME}/.gitconfig-local"
 fi
